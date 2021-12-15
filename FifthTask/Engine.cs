1bb59262a015187f52b62fe217685903a7c371f9
vadikurz <kurzov.2002@gmail.com>
2021-12-15 13:32:16 +0300

    Add project files

---
+++ FifthTask/Engine.cs
@@ -0,0 +1,27 @@
+﻿namespace FifthTask
+{
+    public class Engine
+    {
+        public int Power { get; set; }
+
+        public double Capacity { get; set; }
+
+        public EngineType Type { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public Engine(){}
+        public Engine(int power, double capacity, EngineType type, string serialNumber)
+        {
+            Power = power;
+            Capacity = capacity;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Power}\n{Capacity}\n{Type}\n{SerialNumber}\n";
+        }
+    }
+}
---
+++ FifthTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace FifthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                var car = new Car(new Engine(300, 3, EngineType.Diesel, "1234567v"),
+                    new Transmission(TransmissionType.Automatic, 7, "Aisin"),
+                    new Chassis(4, "12345v2", 1000), CarBody.StationWagon);
+
+                var bus = new Bus(new Engine(155, 2, EngineType.Electric, "v21r332502"),
+                    new Transmission(TransmissionType.Manual, 8, "ZF"),
+                    new Chassis(4, "004522vr34", 2000), 20);
+
+                var scooter = new Scooter(new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
+                    new Transmission(TransmissionType.Manual, 8, "Aisin"),
+                    new Chassis(2, "002245vr99", 200), 70);
+
+                var truck = new Truck(new Engine(200, 3, EngineType.Electric, "dr3407tye"),
+                    new Transmission(TransmissionType.Automatic, 8, "ZF"),
+                    new Chassis(4, "456308ht95", 2500), 5);
+                
+                List<Vehicle> vehicles = new List<Vehicle>() { car, bus, scooter, truck };
+
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Vehicle>));
+                using FileStream firstFileStream = new FileStream("vehicle_volume_more_than1,5.xml", FileMode.OpenOrCreate);
+                serializer.Serialize(firstFileStream, vehicles.Where(vehicle => vehicle.Engine.Capacity > 1.5).ToList());
+
+                serializer = new XmlSerializer(typeof(List<(EngineType, string, int)>));
+                using FileStream secondFileStream = new FileStream("only_bus_and_truck_engine_information.xml", FileMode.OpenOrCreate);
+                serializer.Serialize(secondFileStream, vehicles.Where(vehicle => (vehicle is Truck) || (vehicle is Bus))
+                    .Select(vehicle => vehicle.Engine).Select(engine => (EngineType: engine.Type, SerialNumber: engine.SerialNumber, Power: engine.Power)).ToList());
+
+                serializer = new XmlSerializer(typeof(List<Vehicle>));
+                using FileStream ThirdFileStream = new FileStream("all_information_grouped_by_transmissiontype.xml", FileMode.OpenOrCreate);
+                serializer.Serialize(ThirdFileStream, vehicles.OrderBy(vehicle => vehicle.Transmission.Type).ToList());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
