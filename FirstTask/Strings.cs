ff11ffd8bc2c780d16cd195fdd175c689f667faf
vadikurz <kurzov.2002@gmail.com>
2021-12-15 13:11:52 +0300

    Add project files

---
+++ FirstTask/Strings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+
+namespace FirstTask
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// For example:
+        /// "ggkljg" => 4
+        /// </summary>
+        /// <param name="str" ></param>
+        /// <returns> maximum count of unique sequential characters. </returns>
+        public static int MaxCountOfUniqueSequentialCharacters(this string str)
+        {
+            int max = -1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                HashSet<char> items = new HashSet<char> { str[i] };
+                int j = i + 1;
+                while (j < str.Length && !items.Contains(str[j]))
+                {
+                    items.Add(str[j]);
+                    j++;
+                }
+
+                if (j - i > max)
+                {
+                    max = j - i;
+                }
+            }
+            return max;
+        }
+    }
+}
