a2f62f178be973144d193fe3fdd52bd3e8ea4153
vadikurz <kurzov.2002@gmail.com>
2021-12-16 01:09:25 +0300

    edit creating object

--- FourthTask/Program.cs
+++ FourthTask/Program.cs
@@ -9,7 +9,7 @@ namespace FourthTask
             try
             {
                 var point = new Point(15, 2, 3);
-                Bird bird = new Bird(point, 20);
+                Bird bird = new Bird(point);
                 bird.FlyTo(new Point(100, 20, 35));
                 Console.WriteLine(bird.CurrentPoint);
                 
