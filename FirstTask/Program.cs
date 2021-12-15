c485957553f2bb6ef74efb47cdce4b0212ec0f6e
vadikurz <kurzov.2002@gmail.com>
2021-12-15 13:11:52 +0300

    Add project files

---
+++ FirstTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace FirstTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Console.WriteLine(args[0].MaxCountOfUniqueSequentialCharacters());
+            }
+        }
+    }
+}
