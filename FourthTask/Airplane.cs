e8b2f55f5b165d1a533df95237a7378281bca84d
vadikurz <kurzov.2002@gmail.com>
2021-12-15 13:30:25 +0300

    Add project files

---
+++ FourthTask/Airplane.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace FourthTask
+{
+    public class Airplane : IFlyable
+    {
+        public const int StartingSpeed = 200;
+        public const int MaxSpeed = 900;
+        public const int MaxDistance = 6000;
+
+        public Point CurrentPoint { get; private set; }
+
+        public Airplane(Point currentPoint)
+        {
+            CurrentPoint = currentPoint;
+        }
+        public void FlyTo(Point destinationPoint)
+        {
+            double distance = CurrentPoint.GetDistance(CurrentPoint, destinationPoint);
+            if (distance > MaxDistance)
+            {
+                throw new ArgumentOutOfRangeException("The Airplane cannot fly more than 6000 kilometers");
+            }
+            CurrentPoint = destinationPoint;
+        }
+
+        public double GetFlyTime(Point destinationPoint)
+        {
+            double distance = CurrentPoint.GetDistance(CurrentPoint, destinationPoint);
+            if (distance > MaxDistance)
+            {
+                throw new ArgumentOutOfRangeException("The airplane cannot fly more than 6000 kilometers");
+            }
+            int numberOfSpeedIncreases = (int)(distance / 10);
+            int airplaneSpeed = StartingSpeed;
+            double time = 0;
+            while (airplaneSpeed < MaxSpeed && distance > 10 && numberOfSpeedIncreases > 0)
+            {
+                time += 10.0 / airplaneSpeed;
+                airplaneSpeed += 10;
+                distance -= 10;
+                numberOfSpeedIncreases -= 1;
+            }
+            return time + distance / airplaneSpeed;
+        }
+
+    }
+}
---
+++ FourthTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FourthTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var point = new Point(15, 2, 3);
+                Bird bird = new Bird(point, 20);
+                bird.FlyTo(new Point(100, 20, 35));
+                Console.WriteLine(bird.CurrentPoint);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
