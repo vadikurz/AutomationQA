03f2aaf5fe2d74d085c0cd8eb4ccaa2ff13dbe92
vadikurz <kurzov.2002@gmail.com>
2021-12-15 23:59:06 +0300

    convert unit tests to AAA format

--- SecondTaskTests/NumberConverterTests.cs
+++ SecondTaskTests/NumberConverterTests.cs
@@ -3,10 +3,14 @@ using SecondTask;
 
 namespace SecondTaskTests
 {
+    public record TestCase(string NumberToConvert, int Base, int ConverationResult);
+
     [TestClass]
     public class NumberConverterTests
     {
+
         [TestMethod]
+        [DataRow(new TestCase("-10011", 2, -19))]
         public void TestConvertPositive()
         {
             var converter = new NumberConverter();
