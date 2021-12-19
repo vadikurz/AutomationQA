fed471ea31f0db74e186728aff8c3a35e9974e71
vadikurz <kurzov.2002@gmail.com>
2021-12-19 12:24:32 +0300

    add unit tests project files

---
+++ FourthTaskTests/DroneMethodsTests.cs
@@ -0,0 +1,20 @@
+﻿using FourthTask;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FourthTaskTests
+{
+    [TestClass]
+    public class DroneMethodsTests
+    {
+        [TestMethod]
+        [DataRow(9.1366076855, new[] { 15, 2, 3 }, new[] { 400, 232, 90 }, 50, 1e-6)]
+        public void GetFlyTimeTestPositive(double expectedTime, int[] currentPoint, int[] destinationPoint, int speed, double eps)
+        {
+            var drone = new Drone(new Point(currentPoint[0], currentPoint[1], currentPoint[2]), speed);
+
+            double actualTime = drone.GetFlyTime(new Point(destinationPoint[0], destinationPoint[1], destinationPoint[2]));
+
+            Assert.AreEqual(expectedTime, actualTime, eps);
+        }
+    }
+}
---
+++ FourthTaskTests/PointMethodsTests.cs
@@ -0,0 +1,22 @@
+﻿using FourthTask;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FourthTaskTests
+{
+    [TestClass]
+    public class PointMethodsTests
+    {
+        [TestMethod]
+        [DataRow(792.13950791, new[] { 17, 3, 5 }, new[] { 143, 256, 745 }, 1e-6)]
+        public void GetDistanceTestPositive(double expectedDistance, int[] startingPoint, int[] destination, double eps)
+        {
+            var startPoint = new Point(startingPoint[0], startingPoint[1], startingPoint[2]);
+            var destinationPoint = new Point(destination[0], destination[1], destination[2]);
+
+            double actualDistance = startPoint.GetDistance(startPoint, destinationPoint);
+
+            Assert.AreEqual(expectedDistance, actualDistance, eps);
+        }
+
+    }
+}
