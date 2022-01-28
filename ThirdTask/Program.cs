428881ddbb8750de47d5dbda3316328a6cf7429a
vadikurz <kurzov.2002@gmail.com>
2022-01-28 13:24:52 +0300

    create anothers vehicles

--- ThirdTask/Program.cs
+++ ThirdTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ThirdTask
 {
@@ -9,10 +10,26 @@ namespace ThirdTask
             try
             {
                 var car = new Car(new Engine(300, 3, EngineType.Diesel, "1234567v"),
-                    new Transmission(TransmissionType.Automatic, 7, "mercedes"),
-                    new Chassis(4, "12345v2", 200),
-                    CarBody.StationWagon);
-                Console.WriteLine(car.ToString());
+                    new Transmission(TransmissionType.Automatic, 7, "Aisin"),
+                    new Chassis(4, "12345v2", 1000), CarBody.StationWagon);
+
+                var bus = new Bus(new Engine(155, 2, EngineType.Electric, "v21r332502"),
+                    new Transmission(TransmissionType.Manual, 8, "ZF"),
+                    new Chassis(4, "004522vr34", 2000), 20);
+
+                var scooter = new Scooter(new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
+                    new Transmission(TransmissionType.Manual, 8, "Aisin"),
+                    new Chassis(2, "002245vr99", 200), 70);
+
+                var truck = new Truck(new Engine(200, 3, EngineType.Electric, "dr3407tye"),
+                    new Transmission(TransmissionType.Automatic, 8, "ZF"),
+                    new Chassis(4, "456308ht95", 2500), 5);
+
+                List<Vehicle> vehicles = new List<Vehicle>() { car, bus, scooter, truck };
+                foreach (var vehicle in vehicles)
+                {
+                    Console.WriteLine(vehicle.ToString());
+                }
             }
             catch (Exception ex)
             {
