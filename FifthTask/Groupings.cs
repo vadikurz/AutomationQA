0ac5071ef232fecbe2cf4f8625b0398961f021e5
vadikurz <kurzov.2002@gmail.com>
2022-01-29 00:23:02 +0300

    add grouping extension method for group serialization model

---
+++ FifthTask/Groupings.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace FifthTask
+{
+    public static class Groupings
+    {
+        public static GroupSerializationModel<TKey,TElement> ToSerializationModel<TKey, TElement>(this IGrouping<TKey, TElement> group) =>
+            new GroupSerializationModel<TKey, TElement>(group);
+    }
+}
