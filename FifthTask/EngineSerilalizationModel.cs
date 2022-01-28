9778385d757a4fc0244b37df0c8859822bb42256
vadikurz <kurzov.2002@gmail.com>
2022-01-29 00:21:22 +0300

    add engine serialization model

---
+++ FifthTask/EngineSerilalizationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FifthTask
+{
+    [Serializable]
+    public class EngineSerilalizationModel
+    {
+        public int Power { get; set; }
+
+        public EngineType Type { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public EngineSerilalizationModel() { }
+        public EngineSerilalizationModel(int power, EngineType type, string serialNumber)
+        {
+            Power = power;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+    }
+}
