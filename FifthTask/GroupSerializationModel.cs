beb0221e4b7104ab891017015209c215711df53a
vadikurz <kurzov.2002@gmail.com>
2022-01-29 00:22:02 +0300

    add group serialization model

---
+++ FifthTask/GroupSerializationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace FifthTask
+{
+    [Serializable]
+    [XmlType("GroupedByTransmissionType")]
+    public class GroupSerializationModel<TKey, TElement>
+    {
+        public TKey Key { get; set; }
+        public List<TElement> Elements { get; set; }
+
+        public GroupSerializationModel() { }
+
+        public GroupSerializationModel(IGrouping<TKey, TElement> group)
+        {
+            Key = group.Key;
+            Elements = group.ToList();
+        }
+
+    }
+}
