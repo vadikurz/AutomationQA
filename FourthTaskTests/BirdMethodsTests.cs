625b747ffc219af503ac55e308d620d8c945f709
vadikurz <kurzov.2002@gmail.com>
2022-01-31 22:40:56 +0300

    make delta constant

--- FourthTaskTests/BirdMethodsTests.cs
+++ FourthTaskTests/BirdMethodsTests.cs
@@ -6,16 +6,17 @@ namespace FourthTaskTests
     [TestClass]
     public class BirdMethodsTests
     {
+        public const double Delta = 1e-6;
         [TestMethod]
-        [DataRow(6.1726997515, new[] { 15, 2, 3 }, new[] { 100, 20, 35 }, 15, 1e-6)]
-        public void GetFlyTimeTestPositive(double expectedTime, int[] currentPoint, int[] destinationPoint, int speed, double eps)
+        [DataRow(6.1726997515, new[] { 15, 2, 3 }, new[] { 100, 20, 35 }, 15)]
+        public void GetFlyTimeTestPositive(double expectedTime, int[] currentPoint, int[] destinationPoint, int speed)
         {
             var bird = new Bird(new Point(currentPoint[0], currentPoint[1], currentPoint[2]));
             bird.Speed = speed;
 
             double actualTime = bird.GetFlyTime(new Point(destinationPoint[0], destinationPoint[1], destinationPoint[2]));
 
-            Assert.AreEqual(expectedTime, actualTime, eps);
+            Assert.AreEqual(expectedTime, actualTime, Delta);
         }
     }
 }
