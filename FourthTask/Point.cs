a079f4d401c363992e84eb62e533be84f2d958d2
vadikurz <kurzov.2002@gmail.com>
2022-01-31 20:27:34 +0300

    remove setters in  properties

--- FourthTask/Point.cs
+++ FourthTask/Point.cs
@@ -4,9 +4,9 @@ namespace FourthTask
 {
     public class Point
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Z { get; set; }
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
 
         public Point(int x, int y, int z)
         {
