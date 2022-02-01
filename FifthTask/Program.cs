78a9f7cf2a36d22e6495d36310258b15287cc464
vadikurz <kurzov.2002@gmail.com>
2022-02-01 23:32:49 +0300

    refactor objects creation

--- FifthTask/Program.cs
+++ FifthTask/Program.cs
@@ -10,6 +10,26 @@ namespace FifthTask
 {
     public class Program
     {
+        public static List<Vehicle> CreateVehicles()
+            => new()
+            {
+                new Car(new Engine(300, 3, EngineType.Diesel, "1234567v"),
+                    new Transmission(TransmissionType.Automatic, 7, "Aisin"),
+                    new Chassis(4, "12345v2", 1000), CarBody.StationWagon),
+
+                new Bus(new Engine(155, 2, EngineType.Electric, "v21r332502"),
+                    new Transmission(TransmissionType.Manual, 8, "ZF"),
+                    new Chassis(4, "004522vr34", 2000), 20),
+
+                new Scooter(new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
+                    new Transmission(TransmissionType.Manual, 8, "Aisin"),
+                    new Chassis(2, "002245vr99", 200), 70),
+
+                new Truck(new Engine(200, 3, EngineType.Electric, "dr3407tye"),
+                    new Transmission(TransmissionType.Automatic, 8, "ZF"),
+                    new Chassis(4, "456308ht95", 2500), 5)
+            };
+
         public static IEnumerable<ViewSerializer<ICollection<Vehicle>>> CreateViewSerializers()
             => new ViewSerializer<ICollection<Vehicle>>[]
             {
@@ -43,31 +63,12 @@ namespace FifthTask
         {
             try
             {
-
-                var car = new Car(new Engine(300, 3, EngineType.Diesel, "1234567v"),
-                    new Transmission(TransmissionType.Automatic, 7, "Aisin"),
-                    new Chassis(4, "12345v2", 1000), CarBody.StationWagon);
-
-                var bus = new Bus(new Engine(155, 2, EngineType.Electric, "v21r332502"),
-                    new Transmission(TransmissionType.Manual, 8, "ZF"),
-                    new Chassis(4, "004522vr34", 2000), 20);
-
-                var scooter = new Scooter(new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
-                    new Transmission(TransmissionType.Manual, 8, "Aisin"),
-                    new Chassis(2, "002245vr99", 200), 70);
-
-                var truck = new Truck(new Engine(200, 3, EngineType.Electric, "dr3407tye"),
-                    new Transmission(TransmissionType.Automatic, 8, "ZF"),
-                    new Chassis(4, "456308ht95", 2500), 5);
-
-                List<Vehicle> vehicles = new List<Vehicle>() { car, bus, scooter, truck };
-
+                var vehicles = CreateVehicles();
                 var viewSerializers = CreateViewSerializers();
                 foreach (var decorator in viewSerializers)
                 {
                     decorator.Execute(vehicles);
                 }
-
             }
             catch (Exception ex)
             {
