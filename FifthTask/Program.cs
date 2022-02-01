4f613ca777e8f771f09923ddd8e086d688d32d2f
vadikurz <kurzov.2002@gmail.com>
2022-02-02 00:10:41 +0300

    refactor method CreateVehicles

--- FifthTask/Program.cs
+++ FifthTask/Program.cs
@@ -13,21 +13,37 @@ namespace FifthTask
         public static List<Vehicle> CreateVehicles()
             => new()
             {
-                new Car(new Engine(300, 3, EngineType.Diesel, "1234567v"),
+                new Car
+                (
+                    new Engine(300, 3, EngineType.Diesel, "1234567v"),
                     new Transmission(TransmissionType.Automatic, 7, "Aisin"),
-                    new Chassis(4, "12345v2", 1000), CarBody.StationWagon),
+                    new Chassis(4, "12345v2", 1000), 
+                    bodyType: CarBody.StationWagon
+                ),
 
-                new Bus(new Engine(155, 2, EngineType.Electric, "v21r332502"),
+                new Bus
+                (
+                    new Engine(155, 2, EngineType.Electric, "v21r332502"),
                     new Transmission(TransmissionType.Manual, 8, "ZF"),
-                    new Chassis(4, "004522vr34", 2000), 20),
+                    new Chassis(4, "004522vr34", 2000), 
+                    numberOfSeats: 20
+                ),
 
-                new Scooter(new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
+                new Scooter
+                (
+                    new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
                     new Transmission(TransmissionType.Manual, 8, "Aisin"),
-                    new Chassis(2, "002245vr99", 200), 70),
+                    new Chassis(2, "002245vr99", 200), 
+                    maxSpeed: 70
+                ),
 
-                new Truck(new Engine(200, 3, EngineType.Electric, "dr3407tye"),
+                new Truck
+                (
+                    new Engine(200, 3, EngineType.Electric, "dr3407tye"),
                     new Transmission(TransmissionType.Automatic, 8, "ZF"),
-                    new Chassis(4, "456308ht95", 2500), 5)
+                    new Chassis(4, "456308ht95", 2500), 
+                    liftingCapacity: 5
+                )
             };
 
         public static IEnumerable<ViewSerializer<ICollection<Vehicle>>> CreateViewSerializers()
