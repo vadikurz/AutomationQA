d075b6e5fe495aaef0dbada867475c2eab8d0fc9
vadikurz <kurzov.2002@gmail.com>
2022-02-07 23:34:50 +0300

    Rename test methods

--- FourthTaskTests/DroneMethodsTests.cs
+++ FourthTaskTests/DroneMethodsTests.cs
@@ -9,7 +9,7 @@ namespace FourthTaskTests
         public const double Delta = 1e-6;
         [TestMethod]
         [DataRow(9.1366076855, new[] { 15, 2, 3 }, new[] { 400, 232, 90 }, 50)]
-        public void GetFlyTimeTestPositive(double expectedTime, int[] currentPoint, int[] destinationPoint, int speed)
+        public void GetFlyTimePositiveTest(double expectedTime, int[] currentPoint, int[] destinationPoint, int speed)
         {
             var drone = new Drone(new Point(currentPoint[0], currentPoint[1], currentPoint[2]), speed);
 
