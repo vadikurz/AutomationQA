02d8a19f726a7194464cd0f7f755c56c4315103c
vadikurz <kurzov.2002@gmail.com>
2022-02-12 15:04:31 +0300

    add class CarPark

---
+++ FifthTask/CarPark.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using FifthTask.Vehicles;
+
+namespace FifthTask
+{
+    public class CarPark
+    {
+        public Dictionary<string, Vehicle> Vehicles { get; set; }
+
+        public CarPark()
+        {
+            Vehicles = new Dictionary<string, Vehicle>();
+        }
+    }
+}
