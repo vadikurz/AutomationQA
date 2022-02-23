0c3fda40e2f0d2078696ce127fb2a1976b5ed7cb
vadikurz <kurzov.2002@gmail.com>
2022-02-23 23:14:40 +0300

    Add InitializationExceptions

---
+++ FifthTask/Exceptions/IncorrectValueException.cs
@@ -0,0 +1,10 @@
+﻿namespace FifthTask.Exceptions
+{
+    public class IncorrectValueException : InitializationException
+    {
+        public IncorrectValueException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ FifthTask/Exceptions/InitializationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FifthTask.Exceptions
+{
+    public class InitializationException : ArgumentOutOfRangeException
+    {
+        public InitializationException(string message) : base(message)
+        {
+
+        }
+    }
+}
