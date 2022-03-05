3ce96da9c02eae79e0fdfe04581f2dadf083bfc3
vadikurz <kurzov.2002@gmail.com>
2022-03-06 00:19:08 +0300

    add TestInitialize attribute, refacor

--- ExceptionTaskTests/CarParkTests.cs
+++ ExceptionTaskTests/CarParkTests.cs
@@ -9,12 +9,18 @@ namespace ExceptionTaskTests
     [TestClass]
     public class CarParkTests
     {
-        [TestMethod]
-        [DataRow("123")]
-        public void AddVehicleShouldThrowAddException(string id)
+        private CarPark defaultCarPark;
+        private Car defaultCar;
+        private Bus defaultBus;
+        private Truck defaultTruck;
+        private Scooter defaultScooter;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            var carPark = new CarPark();
-            var car = new Car
+            defaultCarPark = new CarPark();
+
+            defaultCar = new Car
             (
                 new Engine(300, 3, EngineType.Diesel, "1234567v"),
                 new Transmission(TransmissionType.Automatic, 7, "Aisin"),
@@ -22,9 +28,38 @@ namespace ExceptionTaskTests
                 bodyType: CarBody.StationWagon
             );
 
-            carPark.Add(car, id);
+            defaultBus = new Bus
+            (
+                new Engine(155, 2, EngineType.Electric, "v21r332502"),
+                new Transmission(TransmissionType.Manual, 8, "ZF"),
+                new Chassis(4, "004522vr34", 2000),
+                numberOfSeats: 20
+            );
 
-            Assert.ThrowsException<AddException>(() => carPark.Add(car, id));
+            defaultTruck = new Truck
+            (
+                new Engine(200, 3, EngineType.Electric, "dr3407tye"),
+                new Transmission(TransmissionType.Automatic, 8, "ZF"),
+                new Chassis(4, "456308ht95", 2500),
+                liftingCapacity: 5
+            );
+
+            defaultScooter = new Scooter
+            (
+                new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
+                new Transmission(TransmissionType.Manual, 8, "Aisin"),
+                new Chassis(2, "002245vr99", 200),
+                maxSpeed: 70
+            );
+        }
+
+        [TestMethod]
+        [DataRow("123")]
+        public void AddVehicleShouldThrowAddException(string id)
+        {
+            defaultCarPark.Add(defaultCar, id);
+
+            Assert.ThrowsException<AddException>(() => defaultCarPark.Add(defaultCar, id));
         }
 
         [TestMethod]
@@ -32,59 +67,32 @@ namespace ExceptionTaskTests
         [DataRow("null", "null")]
         public void GetAutoByParameterShouldThrowNotFoundParameterException(string parameter, string value)
         {
-            var carPark = new CarPark();
-            var truck = new Truck
-            (
-                new Engine(200, 3, EngineType.Electric, "dr3407tye"),
-                new Transmission(TransmissionType.Automatic, 8, "ZF"),
-                new Chassis(4, "456308ht95", 2500),
-                liftingCapacity: 5
-            );
-            carPark.Add(truck, "123");
+            defaultCarPark.Add(defaultTruck, "123");
 
-            Assert.ThrowsException<NotFoundParameterException>(() => carPark.GetAutoByParameter(parameter, value));
+            Assert.ThrowsException<NotFoundParameterException>(() => defaultCarPark.GetAutoByParameter(parameter, value));
         }
 
         [TestMethod]
         [DataRow("BodyType", "null")]
+        [DataRow("bodytype", "")]
         public void GetAutoByParameterShouldThrowNotFoundValueByParameterException(string parameter, string value)
         {
-            var carPark = new CarPark();
-            var car = new Car
-            (
-                new Engine(300, 3, EngineType.Diesel, "1234567v"),
-                new Transmission(TransmissionType.Automatic, 7, "Aisin"),
-                new Chassis(4, "12345v2", 1000),
-                bodyType: CarBody.StationWagon
-            );
-            carPark.Add(car, "123");
+            defaultCarPark.Add(defaultCar, "123");
 
-            Assert.ThrowsException<NotFoundValueByParameterException>(() => carPark.GetAutoByParameter(parameter, value));
+            Assert.ThrowsException<NotFoundValueByParameterException>(() => defaultCarPark.GetAutoByParameter(parameter, value));
         }
 
         [TestMethod]
         [DataRow("NumberOfSeats", "20")]
         public void GetAutoByParameterShouldThrowAmbiguousMatchException(string parameter, string value)
         {
-            var carPark = new CarPark();
-            var firstBus = new Bus
-            (
-                new Engine(155, 2, EngineType.Electric, "v21r332502"),
-                new Transmission(TransmissionType.Manual, 8, "ZF"),
-                new Chassis(4, "004522vr34", 2000),
-                numberOfSeats: 20
-            );
-            var secondBus = new Bus
-            (
-                new Engine(155, 2, EngineType.Electric, "v21r332502"),
-                new Transmission(TransmissionType.Manual, 8, "ZF"),
-                new Chassis(4, "004522vr34", 2000),
-                numberOfSeats: 20
-            );
-            carPark.Add(firstBus, "123");
-            carPark.Add(secondBus, "124");
+            var firstBus = defaultBus;
+            var secondBus = defaultBus;
 
-            Assert.ThrowsException<AmbiguousMatchException>(() => carPark.GetAutoByParameter(parameter, value));
+            defaultCarPark.Add(firstBus, "123");
+            defaultCarPark.Add(secondBus, "124");
+
+            Assert.ThrowsException<AmbiguousMatchException>(() => defaultCarPark.GetAutoByParameter(parameter, value));
         }
 
         [TestMethod]
@@ -92,17 +100,9 @@ namespace ExceptionTaskTests
         [DataRow("25489")]
         public void RemoveAutoShouldThrowRemoveAutoException(string id)
         {
-            var carPark = new CarPark();
-            var car = new Car
-            (
-                new Engine(300, 3, EngineType.Diesel, "1234567v"),
-                new Transmission(TransmissionType.Automatic, 7, "Aisin"),
-                new Chassis(4, "12345v2", 1000),
-                bodyType: CarBody.StationWagon
-            );
-            carPark.Add(car, "123");
+            defaultCarPark.Add(defaultCar, "123");
 
-            Assert.ThrowsException<RemoveAutoException>(() => carPark.RemoveAuto(id));
+            Assert.ThrowsException<RemoveAutoException>(() => defaultCarPark.RemoveAuto(id));
         }
 
         [TestMethod]
@@ -110,25 +110,9 @@ namespace ExceptionTaskTests
         [DataRow("25489")]
         public void UpdateAutoShouldThrowUpdateAutoException(string id)
         {
-            var carPark = new CarPark();
-            var car = new Car
-            (
-                new Engine(300, 3, EngineType.Diesel, "1234567v"),
-                new Transmission(TransmissionType.Automatic, 7, "Aisin"),
-                new Chassis(4, "12345v2", 1000),
-                bodyType: CarBody.StationWagon
-            );
-            carPark.Add(car, "123");
+            defaultCarPark.Add(defaultCar, "123");
 
-            var scooter = new Scooter
-            (
-                new Engine(40, 0.5, EngineType.Electric, "87tr34wqe"),
-                new Transmission(TransmissionType.Manual, 8, "Aisin"),
-                new Chassis(2, "002245vr99", 200),
-                maxSpeed: 70
-            );
-
-            Assert.ThrowsException<UpdateAutoException>(() => carPark.UpdateAuto(id, scooter));
+            Assert.ThrowsException<UpdateAutoException>(() => defaultCarPark.UpdateAuto(id, defaultScooter));
         }
     }
 }
