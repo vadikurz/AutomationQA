c0ea982ab44e9414ebdc0ce753dd9eada166bc88
vadikurz <kurzov.2002@gmail.com>
2022-03-17 20:28:29 +0300

    add class CarManager

---
+++ ObjectOrientedDesignPrinciplesTask/CarManager.cs
@@ -0,0 +1,19 @@
+﻿using ObjectOrientedDesignPrinciplesTask.Commands;
+
+namespace ObjectOrientedDesignPrinciplesTask
+{
+    public class CarManager
+    {
+        public ICommand Command { get; set; }
+
+        public void SetCommand(ICommand command)
+        {
+            Command = command;
+        }
+
+        public void GetInfo()
+        {
+            Command.Execute();
+        }
+    }
+}
