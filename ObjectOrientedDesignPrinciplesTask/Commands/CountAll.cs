c53e24e0c6a33a6a8acf7ab63b5b9d1fed7fc846
vadikurz <kurzov.2002@gmail.com>
2022-03-17 20:26:55 +0300

    add command count all

---
+++ ObjectOrientedDesignPrinciplesTask/Commands/CountAll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ObjectOrientedDesignPrinciplesTask.Commands
+{
+    public class CountAll : ICommand
+    {
+        private static CountAll instance;
+        public CarPark CarPark { get; set; }
+
+        private CountAll(CarPark carPark)
+        {
+            CarPark = carPark;
+        }
+
+        public static CountAll GetInstance(CarPark carPark) => instance ?? (instance = new CountAll(carPark));
+
+        public void Execute()
+        {
+            Console.WriteLine(CarPark.BatchesOfCars.Sum(batch => batch.Number));
+        }
+    }
+}
