6625d888b04f6fb39b31ad7c6b1066fa6e565e28
vadikurz <kurzov.2002@gmail.com>
2022-03-17 20:55:47 +0300

    add main

---
+++ ObjectOrientedDesignPrinciplesTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ObjectOrientedDesignPrinciplesTask.Commands;
+
+namespace ObjectOrientedDesignPrinciplesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carPark = new CarPark();
+            var carManager = new CarManager();
+
+            while (!carPark.IsCarParkFull)
+            {
+                carPark.FillCarPark();
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter command");
+                var command = Console.ReadLine();
+                switch (command)
+                {
+                    case "count all":
+                    {
+                        carManager.SetCommand(CountAll.GetInstance(carPark));
+                        carManager.GetInfo();
+                    } 
+                        break;
+                    case "count types":
+                    {
+                        carManager.SetCommand(CountTypes.GetInstance(carPark));
+                        carManager.GetInfo();
+                    }
+                        break;
+                }
+
+                if (command == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
