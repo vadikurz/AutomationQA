f3329892f013723b56a6c09c4bcfb932df0cacb5
vadikurz <kurzov.2002@gmail.com>
2022-03-17 20:28:14 +0300

    add model BatchesOfCars

---
+++ ObjectOrientedDesignPrinciplesTask/BatchesOfCars.cs
@@ -0,0 +1,21 @@
+﻿namespace ObjectOrientedDesignPrinciplesTask
+{
+    public struct BatchOfCars
+    {
+        public string Type { get; set; }
+
+        public string Model { get; set; }
+
+        public int Number { get; set; }
+
+        public double Price { get; set; }
+
+        public BatchOfCars(string type, string model, int number, double price)
+        {
+            Type = type;
+            Model = model;
+            Number = number;
+            Price = price;
+        }
+    }
+}
