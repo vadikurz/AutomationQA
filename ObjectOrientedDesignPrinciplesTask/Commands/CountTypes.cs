ea09992851d9e65557ed66e8dd1ea58ac19d6ab8
vadikurz <kurzov.2002@gmail.com>
2022-03-17 20:27:13 +0300

    add command count types

---
+++ ObjectOrientedDesignPrinciplesTask/Commands/CountTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectOrientedDesignPrinciplesTask.Commands
+{
+    public class CountTypes : ICommand
+    {
+        private static CountTypes instance;
+        public CarPark CarPark { get; set; }
+
+        private CountTypes(CarPark carPark)
+        {
+            CarPark = carPark;
+        }
+
+        public static CountTypes GetInstance(CarPark carPark) => instance ?? (instance = new CountTypes(carPark));
+
+        public void Execute()
+        {
+            Console.WriteLine(CarPark.BatchesOfCars.Count);
+        }
+    }
+}
