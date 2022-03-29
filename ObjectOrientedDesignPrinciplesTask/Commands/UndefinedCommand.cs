2241ad856c6c2689542f1b9347be761fe52e5836
vadikurz <kurzov.2002@gmail.com>
2022-03-29 22:18:53 +0300

    add UndefinedCommand

---
+++ ObjectOrientedDesignPrinciplesTask/Commands/UndefinedCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObjectOrientedDesignPrinciplesTask.Commands
+{
+    public class UndefinedCommand : ICommand
+    {
+        public string CommandName { get; set; }
+
+        public UndefinedCommand(string commandName)
+        {
+            CommandName = commandName;
+        }
+
+        public void Execute(CarPark carPark)
+        {
+            Console.WriteLine($"Undefined command: \"{CommandName}\"");
+        }
+    }
+}
