f98798fab04db875e8a48decfb08d59b0df6b4a9
vadikurz <kurzov.2002@gmail.com>
2022-03-29 22:20:54 +0300

    refactor main

--- ObjectOrientedDesignPrinciplesTask/Program.cs
+++ ObjectOrientedDesignPrinciplesTask/Program.cs
@@ -1,25 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ObjectOrientedDesignPrinciplesTask.Commands;
+using ObjectOrientedDesignPrinciplesTask.Creators;
 
 namespace ObjectOrientedDesignPrinciplesTask
 {
     public class Program
     {
-        public static ICommand GetCommand(string[] command, CarPark carPark) => (command[0], command[1]) switch
-        {
-            ("count", "all") => new CountAll(carPark),
-            ("count", "types") => new CountTypes(carPark),
-            ("average", "price") => new AveragePrice(carPark),
-        };
-
-        public static ICommand GetCommandWithThreeWords(string[] command, CarPark carPark) => (command[0], command[1], command[2]) switch
-        {
-            ("average", "price", _) _ when (carPark.BatchesOfCars.Any(batch => batch.Type == command[2])) =>
-            new AveragePriceType(carPark, carPark.BatchesOfCars.Where(batch => batch.Type == command[2]).Select(batch => batch.Type).ToList().First())
-        };
-
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             var carPark = new CarPark();
             var carManager = new CarManager();
@@ -35,57 +24,16 @@ namespace ObjectOrientedDesignPrinciplesTask
                 var commandAsArray = Console.ReadLine()?.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 var command = String.Join(" ", commandAsArray!);
 
-               switch (commandAsArray.Length)
+                var creators = new List<ICreator>()
                 {
-                    case 2:
-                        carManager.SetCommand(GetCommand(commandAsArray, carPark));
-                        carManager.GetInfo();
-                        break;
-                    case 3:
-                        carManager.SetCommand(GetCommandWithThreeWords(commandAsArray, carPark));
-                        carManager.GetInfo();
-                        break;
-                }
-                if (command is "exit")
-                {
-                    break;
-                }
-
-               
-                /*
-                switch (command)
-                    {
-                        case "count all":
-                        {
-                            carManager.SetCommand(CountAll.GetInstance(carPark));
-                            carManager.GetInfo();
-                        }
-                            break;
-                        case "count types":
-                        {
-                            carManager.SetCommand(CountTypes.GetInstance(carPark));
-                            carManager.GetInfo();
-                        }
-                            break;
-                        case "average price":
-                        {
-                            carManager.SetCommand(AveragePrice.GetInstance(carPark));
-                            carManager.GetInfo();
-                        }
-                            break;
-                    }
-
-                
-                    if(carPark.BatchesOfCars.Any(batch => command == "average price" + " " + $"{batch.Type}"))
-                    {
-                        var cars = carPark.BatchesOfCars.Where(batch => commandAsArray[2] == batch.Type).ToList();
-                        foreach (var car in cars)
-                        {
-                            Console.WriteLine(car.Type);
-                        }
-                    }
-                */
-
+                    new AveragePriceCreator(),
+                    new CountAllCreator(),
+                    new AveragePriceTypeCreator(),
+                    new CountTypesCreator(), 
+                };
+                carManager.SetCommand(creators.Select(creator => creator.TryCreate(command))
+                    .SingleOrDefault(createdCommand => createdCommand is not null, new UndefinedCommand(command))!);
+                carManager.GetInfo(carPark);
             }
         }
     }
