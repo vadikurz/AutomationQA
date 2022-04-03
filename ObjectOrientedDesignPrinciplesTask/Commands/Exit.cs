751a7833277e4b9613d401d2ec647926e2ed8734
vadikurz <kurzov.2002@gmail.com>
2022-04-03 14:09:18 +0300

    add command Exit

---
+++ ObjectOrientedDesignPrinciplesTask/Commands/Exit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ObjectOrientedDesignPrinciplesTask.Commands;
+
+public class Exit : ICommand
+{
+    public void Execute(CarPark carPark, Action deactivator)
+    {
+        deactivator.Invoke();
+    }
+}
---
+++ ObjectOrientedDesignPrinciplesTask/Creators/ExitCreator.cs
@@ -0,0 +1,16 @@
+﻿using ObjectOrientedDesignPrinciplesTask.Commands;
+
+namespace ObjectOrientedDesignPrinciplesTask.Creators;
+
+public class ExitCreator : ICreator
+{
+    public ICommand? TryCreate(string command)
+    {
+        if (command is "exit")
+        {
+            return new Exit();
+        }
+
+        return null;
+    }
+}
