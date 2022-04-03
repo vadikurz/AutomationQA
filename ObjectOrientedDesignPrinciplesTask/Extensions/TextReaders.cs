ca16d69a0ad8abea77f524948524d9232a893dbc
vadikurz <kurzov.2002@gmail.com>
2022-04-03 14:10:01 +0300

    Add extension method ReadNotWhiteSpaceLine

---
+++ ObjectOrientedDesignPrinciplesTask/Extensions/TextReaders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ObjectOrientedDesignPrinciplesTask.Extensions;
+
+public static class TextReaders
+{
+    public static string ReadNotWhiteSpaceLine(this TextReader reader)
+    {
+        string? type = null;
+       
+        while (type is null || type == String.Empty)
+        {
+            type = Console.ReadLine();
+            if (type is not null)
+            {
+                type = Regex.Replace(type, @"\s+", " ").Trim();
+            }
+        }
+
+        return type;
+    }
+}
