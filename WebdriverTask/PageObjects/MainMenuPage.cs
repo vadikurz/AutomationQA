bd81f3f97b2b4ec4c65358348f2795878a9c819a
vadikurz <kurzov.2002@gmail.com>
2022-05-11 21:42:00 +0300

    add MainMenuPage object

---
+++ WebdriverTask/PageObjects/MainMenuPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace WebdriverTask.PageObjects
+{
+    public class MainMenuPage
+    {
+        private IWebDriver webDriver;
+
+        private readonly By signInButton = By.XPath("//div[@id = 'mailbox']//button");
+
+        public MainMenuPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public AuthorizationPage SignIn()
+        {
+            webDriver.FindElement(signInButton).Click();
+
+            return new AuthorizationPage(webDriver);
+        }
+    }
+}
