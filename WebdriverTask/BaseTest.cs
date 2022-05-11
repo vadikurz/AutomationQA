d71334aa2d41c553a4d90af1a62e67cd6070afb9
vadikurz <kurzov.2002@gmail.com>
2022-05-11 21:43:24 +0300

    add class BaseTest

---
+++ WebdriverTask/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebdriverTask;
+
+public abstract class BaseTest
+{
+    protected IWebDriver webDriver;
+
+    [OneTimeSetUp]
+    protected void DoBeforeAllTests()
+    {
+        webDriver = new ChromeDriver();
+    }
+    
+    [TearDown]
+    protected void DoAfterEach()
+    {
+        webDriver.Quit();
+    }
+}
