f77ec1dd4c86a6bdbc38a02c01608ca9b71e7f68
vadikurz <kurzov.2002@gmail.com>
2022-05-11 21:43:41 +0300

    add class MailRuBaseTest

---
+++ WebdriverTask/MailRuBaseTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace WebdriverTask;
+
+public class MailRuBaseTest : BaseTest
+{
+    [SetUp]
+    protected void DoBeforeEach()
+    {
+        webDriver.Navigate().GoToUrl("https://mail.ru");
+        webDriver.Manage().Window.Maximize();
+    }
+}
