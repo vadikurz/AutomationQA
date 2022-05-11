c7995a2b9fdadeaedd733e55f188d95b9263071e
vadikurz <kurzov.2002@gmail.com>
2022-05-11 21:42:55 +0300

    add MailBoxPage page object

---
+++ WebdriverTask/PageObjects/MailBoxPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace WebdriverTask.PageObjects;
+
+public class MailBoxPage
+{
+    private IWebDriver webDriver;
+
+    private By userLogin = By.XPath("//div[@data-testid = 'whiteline-account']");
+
+    public MailBoxPage(IWebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+    }
+
+    public string UserLogin()
+    {
+        var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        wait.Until(ExpectedConditions.ElementIsVisible(userLogin));
+        return webDriver.FindElement(userLogin).Text;
+    }
+}
