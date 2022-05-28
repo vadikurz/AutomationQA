2e5b1bce9ab995deaf8b316f6b37d99c49afc814
vadikurz <kurzov.2002@gmail.com>
2022-05-28 18:47:50 +0300

    add SendEmail method

--- WebdriverTask/PageObjects/MailBoxPage.cs
+++ WebdriverTask/PageObjects/MailBoxPage.cs
@@ -10,6 +10,8 @@ public class MailBoxPage
     private IWebDriver webDriver;
 
     private By userLogin = By.XPath("//div[@data-testid = 'whiteline-account']");
+    private By NewEmailButton = By.XPath("//span[@class = 'compose-button__wrapper']");
+    
 
     public MailBoxPage(IWebDriver webDriver)
     {
@@ -19,7 +21,20 @@ public class MailBoxPage
     public string UserLogin()
     {
         var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        
         wait.Until(ExpectedConditions.ElementIsVisible(userLogin));
+        
         return webDriver.FindElement(userLogin).Text;
+    }
+
+    public NewEmailPage EnterNewEmailButton()
+    {
+        var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        
+        wait.Until(ExpectedConditions.ElementIsVisible(NewEmailButton));
+        
+        webDriver.FindElement(NewEmailButton).Click();
+        
+        return new NewEmailPage(webDriver);
     }
 }
---
+++ WebdriverTask/PageObjects/NewEmailPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace WebdriverTask.PageObjects;
+
+public class NewEmailPage
+{
+    private IWebDriver webDriver;
+
+    private readonly By textInput = By.XPath("//div[contains(@class, 'editable-container')]//br");
+    private readonly By recipientInput = By.XPath("//div[contains(@class, 'contactsContainer')]//input");
+    private readonly By sendButton = By.XPath("//button[@data-test-id = 'send']");
+    public NewEmailPage(IWebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+    }
+
+    public MailBoxPage SendEmail(string recipient, string text)
+    {
+        var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        
+        wait.Until(ExpectedConditions.ElementIsVisible(recipientInput));
+        
+        webDriver.FindElement(recipientInput).SendKeys(recipient);
+        webDriver.FindElement(textInput).SendKeys(text);
+        webDriver.FindElement(sendButton).Click();
+
+        return new MailBoxPage(webDriver);
+    }
+}
