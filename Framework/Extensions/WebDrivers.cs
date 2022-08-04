ace8c6f6b7f47f2931ca6a7e32c7642b714cc51c
vadikurz <kurzov.2002@gmail.com>
2022-08-04 22:57:08 +0300

    add extension methods SwitchToPreviousTab and SwitchToNextTab

---
+++ Framework/Extensions/WebDrivers.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+
+namespace Framework.Extensions;
+
+public static class WebDrivers
+{
+    public static void SwitchToPreviousTab(this IWebDriver driver)
+    {
+        var currentWindowHandleIndex = driver.CurrentWindowHandleIndex();
+        var windowHandles = driver.WindowHandles;
+
+        if (currentWindowHandleIndex is not 0)
+        {
+            driver.SwitchTo().Window(windowHandles[currentWindowHandleIndex - 1]);
+        }
+    }
+
+    public static void SwitchToNextTab(this IWebDriver driver)
+    {
+        var currentWindowHandleIndex = driver.CurrentWindowHandleIndex();
+        var windowHandles = driver.WindowHandles;
+        if (currentWindowHandleIndex != windowHandles.Count - 1)
+        {
+            driver.SwitchTo().Window(windowHandles[currentWindowHandleIndex + 1]);
+        }
+    }
+
+    private static int CurrentWindowHandleIndex(this IWebDriver driver)
+    {
+        var tabs = driver.WindowHandles;
+        
+        return tabs.IndexOf(driver.CurrentWindowHandle);
+    }
+}
