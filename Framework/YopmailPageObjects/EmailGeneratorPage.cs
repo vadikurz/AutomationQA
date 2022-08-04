4b22711960e56a87fd235141b2d21d531470cca5
vadikurz <kurzov.2002@gmail.com>
2022-08-04 22:57:56 +0300

    add YopMail page objects

---
+++ Framework/YopmailPageObjects/EmailGeneratorPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Framework.YopMailPageObjects;
+
+public class EmailGeneratorPage
+{
+    private IWebDriver webDriver;
+
+    private readonly By generatedEmailAddress = By.XPath("//div[@id='egen']");
+    private readonly By checkMailButton = By.XPath("//span[text()='Проверить почту']//parent::button");
+
+    public EmailGeneratorPage(IWebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+    }
+
+    public string CopyGeneratedEmailAddress()
+    {
+        var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+
+        return wait.Until(ExpectedConditions.ElementIsVisible(generatedEmailAddress)).Text;
+    }
+
+    public MailBoxPage ClickButtonCheckMail()
+    {
+        webDriver.FindElement(checkMailButton).Click();
+
+        return new MailBoxPage(webDriver);
+    }
+}
---
+++ Framework/YopmailPageObjects/YopMailMainPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Framework.YopMailPageObjects;
+
+public class YopMailMainPage
+{
+    private IWebDriver webDriver;
+
+    private readonly By emailGeneratorButton = By.XPath("//div[@id='listeliens']//a[@href = 'email-generator']");
+
+    public YopMailMainPage(IWebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+    }
+
+    public EmailGeneratorPage ClickButtonGenerateEmail()
+    {
+        var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+
+        wait.Until(ExpectedConditions.ElementIsVisible(emailGeneratorButton)).Click();
+        
+        return new EmailGeneratorPage(webDriver);
+    }
+}
