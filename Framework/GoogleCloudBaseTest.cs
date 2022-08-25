874ad7591b6537737cbc6fd3077af71c8e3d0836
vadikurz <kurzov.2002@gmail.com>
2022-08-25 13:19:06 +0300

    add TearDown method DoAfterEach in GoogleCloudBaseTest class, add the ability to take a screenshot when the test failed

--- Framework/GoogleCloudBaseTest.cs
+++ Framework/GoogleCloudBaseTest.cs
@@ -1,10 +1,15 @@
-﻿using Framework.Services;
+﻿using System;
+using Framework.Services;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
 
 namespace Framework
 {
-    public class GoogleCloudBaseTest : BaseTest
+    public class GoogleCloudBaseTest 
     {
+        protected IWebDriver webDriver;
+        
         [SetUp]
         protected void DoBeforeEach()
         {
@@ -13,6 +18,21 @@ namespace Framework
             webDriver.Manage().Cookies.DeleteAllCookies();
             webDriver.Navigate().GoToUrl("https://cloud.google.com/");
             webDriver.Manage().Window.Maximize();
+        }
+        
+        [TearDown]
+        protected void DoAfterEach()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                var screenshot = ((ITakesScreenshot)webDriver).GetScreenshot();
+                
+                screenshot.SaveAsFile(".//screenshots/" + $"{DateTime.Now:dd-MM-yyyy_HH-mm-ss.fffff}.jpg",ScreenshotImageFormat.Jpeg);
+                screenshot.SaveAsFile("C:/ProgramData/Jenkins/.jenkins/workspace/Framework/screenshots/" +
+                                      $"{DateTime.Now:dd-MM-yyyy_HH-mm-ss.fffff}.jpg",ScreenshotImageFormat.Jpeg);
+            }
+
+            Driver.CloseDriver();
         }
     }
 }
