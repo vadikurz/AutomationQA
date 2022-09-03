b597eb04d71fe9ae5addbadfcb3e2805d2f2f711
vadikurz <kurzov.2002@gmail.com>
2022-09-03 22:56:12 +0300

    add abstract page class

---
+++ Framework/PageObjects/AbstractPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace Framework.PageObjects;
+
+public abstract class AbstractPage
+{
+    protected IWebDriver webDriver;
+    protected const int WaitTimeOut = 10;
+
+    protected AbstractPage(IWebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+    }
+}
